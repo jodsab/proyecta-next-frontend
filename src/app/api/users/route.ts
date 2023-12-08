733a3a0377df3cec9303c0f34e272bbabf2e980c
jodsab <camilo.b.q@hotmail.com>
2023-12-08 15:47:02 -0500

    fixed an error on users api

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,20 +1,5 @@
 import { NextResponse } from "next/server";
 
-export function GET() {
-  return NextResponse.json({ sda: "GET world" });
-}
-
 export async function POST(request) {
   const data = await request.json();
-
-
-}
-
-
-export function PUT() {
-  return NextResponse.json({ sda: "PUT world" });
-}
-
-export function DELETE() {
-  return NextResponse.json({ sda: "DELETE world" });
 }
