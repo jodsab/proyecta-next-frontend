418282b13e5668063e6124d108f56d1c50cadcd5
jodsab <camilo.b.q@hotmail.com>
2024-05-16 17:50:32 -0500

    fix: new projects

---
+++ src/app/proyectos/huerto-urbano/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import "./index.scss";
+
+export default function HuertoUrbano() {
+  return (
+    <div className="homepage_container">
+      <h1>Bosque Alto</h1>
+    </div>
+  );
+}
